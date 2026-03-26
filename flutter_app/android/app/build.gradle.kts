9d4547c32f0bd2565c527563c9e371f46eb13fb3
jesus caro <j.caro@lobbypms.com>
2026-03-26 14:24:30 -0500

    feat: add missing flutter source files

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -32,10 +32,12 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it as String) }
-            storePassword = keystoreProperties["storePassword"] as String
+            if (keystorePropertiesFile.exists()) {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = keystoreProperties["storeFile"]?.let { file(it as String) }
+                storePassword = keystoreProperties["storePassword"] as String
+            }
         }
     }
 
